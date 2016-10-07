14de735d9fbfca66b59802cf05ed74b833f150f6
codebooker <codebooker@gmail.com>
2016-10-07 01:23:15 -0400

    Fixed Preloader? (hopefully)

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,10 @@
 $(window).load(function () {
 
     // preloader
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(550).css({
-        'overflow': 'visible'
-    });
+    (function () {
+        $('#status').fadeOut();
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
 
 
     //  isotope
